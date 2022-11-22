2e00f6b6a5094635e80af407c9ce85c21691581f
william7829 <92896220+william7829@users.noreply.github.com>
2022-11-22 01:02:50 -0600

    do not merge this

--- front-end/src/components/Pages/ProductDetails/ProductDetails.js
+++ front-end/src/components/Pages/ProductDetails/ProductDetails.js
@@ -9,101 +9,112 @@ import { useParams } from 'react-router-dom';
 
 
 
-function ProductDetails(props) {
 
-  const { id } = useParams();
+  
 
 
-  const [posts, setPosts] = useState([]);
-  console.log(props);
-  console.log("above is the productDetails Props");
 
+
+
+  function ProductDetails(props) {
+    const [posts,setPosts] = useState([]);
+    const [products,setProducts] = useState([]);
+    console.log(props);
+    console.log("above is the productDetails Props")
+  
   let url = window.location.pathname;
-
+  
+  let productId = url.split("/").pop();
+  
+  console.log(productId);
+  
   useEffect(()=>{
-    getDetails(id).then(data=>{
-
+    getDetails(productId).then(data=>{
+  
       //setPosts(data);
-      console.log("below is the data product details is rendering");
+      console.log("below is the data product details is rendering")
       console.log(data);
+      data.map(item=>{
+  
+          return item;
+      });
       setPosts(data);
-    });
-  }, []);
-
-
-=======
-  return (
-    <DetailsProduct id={id} post={posts} />
-  );
-
-
-  const getProducts = async ()=>{
-    let result = await fetch('https://localhost:5000/api/product')
-    result= await result.json;
-    setProducts(result);
+  })
+  },[])
+  
+  
+  const deleteProduct = (item) =>{
+    console.warn(item._id)
   }
   
-    /*
-    return(
-      <div class="pageContent">
-<div>
-
-  <Link to={`/edit/product/${id}`}>Edit</Link>
-  <Link>Delete</Link>
-
-</div>
-
-<div class="details-wrapper">
-  <div class="details-card">
-    <div class="product-left">
-      <div class="details-header">
-        <h1>{props.name}</h1>
-        <h2>Sit down in modern</h2>
-      </div>
-
-      <div class="product-main">
-        <div class="focus">
-          <span>Description</span>
-          <span>Details</span>
-        </div>
-        <p>{props.description}</p>
-      </div>
+    const getProducts = async ()=>{
+      let result = await fetch('https://localhost:5000/api/product')
+      result= await result.json;
+      setProducts(result);
+    }
+    
   
-      <div class="product-details">
+      return(   
+         
+
+        <div class="pageContent">
         
-
-        <div class="product-total">
-          <h3>Total Price</h3>
-          <p>${props.price}</p>
+        {/* <DetailsProduct id={id} post={posts} /> */}
+  <div>
+    <Link to={`/edit/product/${productId}`}>Edit</Link>
+    <Link><span >Delete</span></Link>
+  </div>
+  
+  <div class="details-wrapper">
+    <div class="details-card">
+      <div class="product-left">
+        <div class="details-header">
+          <h1>{props.name}</h1>
+          <h2>Sit down in modern</h2>
+        </div>
+  
+        <div class="product-main">
+          <div class="focus">
+            <span>Description</span>
+            <span>Details</span>
+          </div>
+          <p>{props.description}</p>
+        </div>
+    
+        <div class="product-details">
+          
+  
+          <div class="product-total">
+            <h3>Total Price</h3>
+            <p>${props.price}</p>
+          </div>
+        </div>
+  
+        <div class="product-btns">
+          <a href="#" class="product-add">Add To Cart</a>
         </div>
       </div>
-
-      <div class="product-btns">
-        <a href="#" class="product-add">Add To Cart</a>
+      <div class="product-right">
+        <img src="{props.imageURL}" alt=""></img>
       </div>
     </div>
-    <div class="product-right">
-      <img src="{props.imageURL}" alt=""></img>
-    </div>
+  {/* {
+    products.map((item,index)=>
+    
+   <ul key={item}>
+    <li>{index+1}</li>
+    <li>{item.name+1}</li>
+    <li>{item.description}</li>
+    <li>{item.imageURL}</li>
+    <li>{item.vendor}</li>
+    <li><button onClick={()=>deleteProduct(item._id)}>Delete</button></li>
+   </ul>
+    
+    )} */}
   </div>
-{/* {
-  products.map((item,index)=>
   
- <ul key={item}>
-  <li>{index+1}</li>
-  <li>{item.name+1}</li>
-  <li>{item.description}</li>
-  <li>{item.imageURL}</li>
-  <li>{item.vendor}</li>
-  <li><button onClick={()=>deleteProduct(item._id)}>Delete</button></li>
- </ul>
+            </div>
+       );
+    }
   
-  )} */}
-</div>
-
-          </div>
-     );
-     */
-  }
-
-  export default ProductDetails;
+    export default ProductDetails;
